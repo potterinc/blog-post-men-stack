fc21661b7b79a2e2d9b1bf651d73af4fba72bd9c
Arekhandia Harry <harry_p30@yahoo.com>
2022-10-12 20:49:59 +0100

    Setup runtime environment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Set static folder
+app.use(express.static(`${__dirname}/public`));
 
-
-app.listen(PORT);
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
