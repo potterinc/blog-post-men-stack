b1b29133393e05ab36f0e7c84b47b66e0e5f5df4
Arekhandia Harry <harry_p30@yahoo.com>
2022-10-14 03:45:21 +0100

    Added DotEnv and Mongoose to project environment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -5,4 +6,4 @@ const PORT = process.env.PORT || 3000;
 // Set static folder
 app.use(express.static(`${__dirname}/public`));
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, (error)=> console.log(`Server running on port ${PORT}`));
